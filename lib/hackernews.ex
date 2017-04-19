1b6060731a2618087cc0561d6685cc5ddff2ee14
John <johnblo@kth.se>
2017-04-19 02:14:30 +0200

    Refactor to avoid downloading stories twice and avoid storing the intermediate JSON

--- lib/hackernews.ex
+++ lib/hackernews.ex
@@ -1,7 +1,29 @@
 defmodule Hackernews do
-  def get_body!(url) do
-    %HTTPoison.Response{status_code: 200, body: body} = HTTPoison.get!(url, [], hackney: [:insecure])
-    body
+  def main do
+    topstories = download_topstories
+
+    sha256 = topstories
+    |> Enum.map(&:erlang.integer_to_binary/1)
+    |> hash
+    |> Integer.to_string(16)
+    |> String.downcase
+
+    case need_update?(sha256) do
+      :true ->
+        # TODO(john) number of and size of chunks should be configurable.
+        chunks = Enum.chunk(topstories, 25, 20, [])
+        text = pmap(chunks, &download_stories/1)
+        File.write!(txt_path, text)
+        File.write!(sha256_path, sha256)
+        IO.puts text
+      :false ->
+        IO.puts File.read!(txt_path)
+    end
+  end
+
+  def download_stories(stories) do
+    Enum.map(stories, &download_story/1)
+    |> Enum.reject(&Enum.empty?/1)
   end
 
   def download_story(id) do
@@ -16,26 +38,32 @@ defmodule Hackernews do
   end
   def format_story(_), do: [] # Ignore stories with no URL
 
-  def download_stories(stories) do
-    Enum.map(stories, &download_story/1)
-    |> Enum.reject(&Enum.empty?/1)
-  end
-
-  def pmap(collection, func, timeout_ms \\ 10_000) do
-    collection
-    |> Enum.map(&(Task.async(fn -> func.(&1) end)))
-    |> Enum.map(fn t -> Task.await(t, timeout_ms) end)
-  end
-
-  def get_stories do
-    data = "https://hacker-news.firebaseio.com/v0/topstories.json"
+  def download_topstories do
+    "https://hacker-news.firebaseio.com/v0/topstories.json"
     |> get_body!
     |> Poison.Parser.parse!
     |> Enum.sort # To avoid cache becoming invalid due to reordering of stories!
-    |> Enum.chunk(25, 20, []) # TODO(john) should be configurable
-    |> pmap(&download_stories/1)
-    
-    File.write!("topstories.txt", data)
+  end
+
+  def home_path(name) do
+     Path.absname(System.user_home!) |> Path.join(name)
+  end
+  def sha256_path do
+    home_path("topstories.sha256")
+  end
+  def txt_path do
+    home_path("topstories.txt")
+  end
+
+  def need_update?(sha256) do
+    case File.read(sha256_path) do
+      {:error, :enoent} -> :true
+      {:ok, local} ->
+        case String.equivalent?(sha256, local) do
+          :true -> :false
+          :false -> :true
+        end
+    end
   end
 
   def hash(data) do
@@ -43,43 +71,14 @@ defmodule Hackernews do
       x
   end
 
-  def topstories do
-    body = "https://hacker-news.firebaseio.com/v0/topstories.json"
-    |> get_body!
-    |> Poison.Parser.parse!
-    |> Enum.sort # To avoid cache becoming invalid due to reordering of stories!
-    |> Enum.map(&:erlang.integer_to_binary/1)
-
-    sha256 = body
-    |> hash
-    |> Integer.to_string(16)
-    |> String.downcase
-
-    {sha256, body}
+  def get_body!(url) do
+    %HTTPoison.Response{status_code: 200, body: body} = HTTPoison.get!(url, [], hackney: [:insecure])
+    body
   end
 
-  def need_update? do
-    {sha256, body} = topstories
-    store = fn ->
-      File.write!("topstories.sha256", sha256)
-      File.write!("topstories.json", body)
-      :true
-    end
-    case File.read("topstories.sha256") do
-      {:error, :enoent} -> store.()
-      {:ok, local} ->
-        case String.equivalent?(sha256, local) do
-          :true -> :false
-          :false -> store.()
-        end
-    end
-  end
-
-  def main do
-    HTTPoison.start
-    case need_update? do
-      :true -> get_stories
-      :false -> :ok
-    end
+  def pmap(collection, func, timeout_ms \\ 10_000) do
+    collection
+    |> Enum.map(&(Task.async(fn -> func.(&1) end)))
+    |> Enum.map(fn t -> Task.await(t, timeout_ms) end)
   end
 end
